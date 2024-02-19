23a52b5e47896d3b9d080df981631a9e52b45aa5
Philip Phyoe <pyaiephyoe@efectivee.com>
2024-02-19 15:43:23 +0630

    add footer tsx and modify button adding one more prop.

--- mystore/app/layout.tsx
+++ mystore/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export const metadata: Metadata = {
   title: "My First Store with Next JS",
@@ -14,9 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="relative">
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
